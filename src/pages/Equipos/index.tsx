bf5e0f4a9f00cf4edb0d7be12021e6eefa610639
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-12-10 23:38:37 -0500

    add: paging buttons and more functions

--- src/pages/Equipos/index.tsx
+++ src/pages/Equipos/index.tsx
@@ -1,81 +1,90 @@
-// import { Box } from '@mui/system'
-import { Button, Grid, TextField, Typography } from '@mui/material'
+import { Box } from '@mui/system'
+import { Button, Grid, LinearProgress, TextField, Typography } from '@mui/material'
 import { ChangeEvent, useEffect, useState } from 'react'
 import { fetchApiTeam } from 'services/fetchApiPlayers'
 import PlayerCard from '../../components/PlayerCards'
 
 const index = () => {
-  const [page, setPage] = useState(1)
+  const [loading, setLoading] = useState(false)
   const [teamName, setTeamName] = useState<string | undefined>(undefined)
   const [players, setPlayers] = useState<any>([])
+  const [page, setPage] = useState(1)
+  const [totalPagesApi, setTotalPages] = useState(1)
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => setTeamName(e.target.value)
 
+  const handlePaginator = (num: number) => {
+    if (page + num > 0 && page + num <= totalPagesApi) {
+      setPage(page + num)
+      window.scrollTo(0, 85)
+    }
+  }
+
   useEffect(() => {
     ;(async () => {
+      setLoading(true)
       if (teamName) {
-        const { data } = await fetchApiTeam(teamName, page)
-        if (data) setPlayers(data)
-        else setPlayers([])
+        const { totalPages, data } = await fetchApiTeam(teamName, page)
+        if (data && totalPages) {
+          setPlayers(data)
+          setTotalPages(totalPages)
+        } else setPlayers([])
       }
+      setLoading(false)
     })()
     return () => {}
   }, [teamName, page])
 
   return (
     <>
-      <Grid
-        container
-        direction="row"
-        spacing={1}
-        sx={{
-          mx: 'auto',
-          width: 500,
-          pb: 3,
-        }}
-      >
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            label="Equipo"
-            id="input-text-equipo"
-            size="small"
-            value={teamName}
-            onChange={handleChange}
-          />
-        </Grid>
-      </Grid>
-      {players.length <= 0 && <Typography>No se encontraron jugadores.</Typography>}
+      <Box display="flex" alignItems="center" justifyContent="center" marginTop={2} marginBottom={3}>
+        <TextField
+          fullWidth
+          label="Equipo"
+          id="input-text-equipo"
+          size="small"
+          value={teamName}
+          onChange={handleChange}
+          sx={{
+            width: {
+              xs: 500,
+              sm: 400,
+              md: 500,
+              lg: 600,
+              pb: 3,
+            },
+            mx: 'auto',
+          }}
+        />
+      </Box>
+      {loading && <LinearProgress />}
+
+      {!teamName ? (
+        <Typography>Realiza una búsqueda por nombre de equipo.</Typography>
+      ) : (
+        !loading &&
+        teamName &&
+        players.length === 0 && <Typography>No se encontraron jugadores en el equipo {teamName}.</Typography>
+      )}
+
       <PlayerCard list={players} />
       {players.length > 0 && (
-        <>
-          <Grid container direction="row" spacing={4}>
-            <Grid item>
-              <Button onClick={() => setPage(page - 1)} size="small">
+        <Grid container direction="row" alignItems="center" justifyContent="center" spacing={1} padding={3}>
+          <Grid item>
+            {page > 1 && (
+              <Button onClick={() => handlePaginator(-1)} size="small">
                 Pagina anterior
               </Button>
-            </Grid>
-            <Grid item>
-              <Button onClick={() => setPage(page + 1)} size="small">
+            )}
+          </Grid>
+          <Grid item>
+            {page < totalPagesApi && (
+              <Button onClick={() => handlePaginator(+1)} size="small">
                 Pagina siguiente
               </Button>
-            </Grid>
+            )}
           </Grid>
-
-          {/* <Box
-            display="flex"
-            width="100%"
-            height={70}
-            marginTop={2}
-            bgcolor="#c8e4fb"
-            alignItems="center"
-            justifyContent="center"
-          >
-            <Button onClick={() => setPage(page + 1)} size="small">
-              Mostrar más
-            </Button>
-          </Box> */}
-        </>
+        </Grid>
       )}
     </>
   )
