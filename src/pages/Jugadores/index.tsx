f24def51f2327ab2ed650631df74ba7dce7adb3b
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-12-10 23:37:26 -0500

    add: loading indicator

--- src/pages/Jugadores/index.tsx
+++ src/pages/Jugadores/index.tsx
@@ -1,33 +1,39 @@
 import PlayerCard from 'components/PlayerCards'
 import { Box } from '@mui/system'
-import { Button } from '@mui/material'
+import { Button, LinearProgress } from '@mui/material'
 import { fetchApiPlayers } from 'services/fetchApiPlayers'
 import { useEffect, useState } from 'react'
 
 const Jugadores = () => {
+  const [loading, setLoading] = useState(false)
   const [page, setPage] = useState(1)
   const [players, setPlayers] = useState<any>([])
 
   useEffect(() => {
     ;(async () => {
+      setLoading(true)
       const { data } = await fetchApiPlayers(page)
       setPlayers((p: any) => [...p, ...data])
+      setLoading(false)
     })()
   }, [page])
 
   return (
     <>
       <PlayerCard list={players} />
-      <Box
-        display="flex"
-        width="100%"
-        height={70}
-        marginTop={2}
-        bgcolor="#c8e4fb"
-        alignItems="center"
-        justifyContent="center"
-      >
-        <Button onClick={() => setPage(page + 1)} size="small">
+      {loading && (
+        <Box marginBottom={3} marginTop={3}>
+          <LinearProgress />
+        </Box>
+      )}
+      <Box display="flex" marginTop={2} alignItems="center" justifyContent="center">
+        <Button
+          disabled={loading}
+          onClick={() => setPage(page + 1)}
+          size="small"
+          fullWidth
+          sx={{ boxShadow: 5, height: 70, backgroundColor: '#c8e4fb' }}
+        >
           Mostrar más
         </Button>
       </Box>
